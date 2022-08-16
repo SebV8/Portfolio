384df4a8a3d1beeb2ee107c048750ff8b726eeb7
SebV8 <99446585+SebV8@users.noreply.github.com>
2022-08-16 16:06:13 -0400

    Updates

---
+++ precache-manifest.d93ac981387a116add938ea1cbb93c66.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0ced6ed7841993f8069",
+    "url": "/react-portfolio/static/css/main.6aeeb707.chunk.css"
+  },
+  {
+    "revision": "b0ced6ed7841993f8069",
+    "url": "/react-portfolio/static/js/main.b0ced6ed.chunk.js"
+  },
+  {
+    "revision": "05fe096ac7e1d0288bcd",
+    "url": "/react-portfolio/static/js/1.05fe096a.chunk.js"
+  },
+  {
+    "revision": "f8926043fa4459ec0101",
+    "url": "/react-portfolio/static/js/runtime~main.f8926043.js"
+  },
+  {
+    "revision": "092676ff20dc4547a49ae435bb685d07",
+    "url": "/react-portfolio/static/media/CV.092676ff.pdf"
+  },
+  {
+    "revision": "0e28123ce297465916aaed9314f7ab34",
+    "url": "/react-portfolio/static/media/me.0e28123c.jpg"
+  },
+  {
+    "revision": "53be3cef357262e93addd90cc15f497a",
+    "url": "/react-portfolio/static/media/about.53be3cef.jpg"
+  },
+  {
+    "revision": "6f60d6f745ad306608122aa71a5b71f2",
+    "url": "/react-portfolio/static/media/night.6f60d6f7.png"
+  },
+  {
+    "revision": "40b039f71b93e8492ed3216378d7e8ec",
+    "url": "/react-portfolio/static/media/night1.40b039f7.jpg"
+  },
+  {
+    "revision": "09bd29b6224d3aa21ffaae95e86467a9",
+    "url": "/react-portfolio/static/media/back.09bd29b6.jpg"
+  },
+  {
+    "revision": "97b858bf22adcd0f0e79a4d6fb108e93",
+    "url": "/react-portfolio/index.html"
+  }
+];
